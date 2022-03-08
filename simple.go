254598379e8f290469733763299cc5c64a56c208
d-darwin <d@darwin.st>
2022-03-08 15:50:17 +0300

    add closer

--- simple.go
+++ simple.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func init() {
+	fmt.Println("Initialization...")
+}
 
 func main() {
+	myTimer := getTimer()
+	defer myTimer()
 	showMeTheMoney()
 	fmt.Println(sum([]int{1, 2, 3}...))
 }
@@ -16,4 +25,11 @@ func sum(argList ...int) (res int) {
 		res += arg
 	}
 	return
+}
+
+func getTimer() func() {
+	start := time.Now()
+	return func() {
+		fmt.Println("Time from start %v", time.Since(start))
+	}
 }
