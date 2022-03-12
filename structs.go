e6ca8ef2717798bcf0440b080e06d78c5bb2092a
d-darwin <d@darwin.st>
2022-03-12 18:10:03 +0300

    add different types of struct init

--- structs.go
+++ structs.go
@@ -10,6 +10,14 @@ type example struct {
 
 func main() {
 	var e1 example
-
 	fmt.Printf("%+v\n", e1)
+
+	e2 := example{
+		Flag:    true,
+		counter: 37,
+	}
+	fmt.Printf("%+v\n", e2)
+
+	e3 := example{false, 33, 1} // Should fill all the fields
+	fmt.Printf("%+v\n", e3)
 }
